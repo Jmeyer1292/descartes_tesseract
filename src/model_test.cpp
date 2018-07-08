c8818ad9d569a44597e09678b09d433b905da5c5
Jonathan Meyer <jmeyer@swri.org>
2018-07-08 18:28:38 -0500

    Added skeleton for off-line test of tesseract / descartes / opw model

---
+++ src/model_test.cpp
@@ -0,0 +1,8 @@
+#include "opw_kinematics/opw_parameters_examples.h"
+#include "descartes_tesseract/tesseract_state_adapter.h"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "model_test");
+  return 0;
+}
