bb40f4d5403e0bf5294418dad4b1f471e68ddeb7
Jonathan Meyer <jmeyer@swri.org>
2018-07-08 18:33:22 -0500

    Fixup

--- src/model_test.cpp
+++ src/model_test.cpp
@@ -4,5 +4,21 @@
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "model_test");
+
+  const auto params = opw_kinematics::makeIrb2400_10<double>();
+
+  // Construct model
+  descartes_tesseract::TesseractStateAdapter model (params, "base_link", "tool0");
+
+  // Initialize
+  if (!model.initialize("robot_description", "manipulator", "base_link", "tool0"))
+  {
+    ROS_ERROR("Failed to initialize robot model");
+    return 1;
+  }
+
+  std::vector<std::vector<double>> joint_poses;
+  model.getAllIK(Eigen::Affine3d::Identity() * Eigen::Translation3d(1.0, 0, 0.5), joint_poses);
+
   return 0;
 }
